f99d7aae37cfa9d7021c3d36bab819c01fd3cc38
Jonathan Piaget <piagetjonathan@protonmail.com>
2026-05-25 07:39:33 +0200

    chore: point firefox runner at developer edition on macos

--- web-ext.config.ts
+++ web-ext.config.ts
@@ -2,4 +2,8 @@ import { defineWebExtConfig } from 'wxt';
 
 export default defineWebExtConfig({
 	chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
+	binaries: {
+		firefox:
+			'/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox',
+	},
 });
