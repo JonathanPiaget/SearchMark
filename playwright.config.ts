20152aa3319eb63fea50f36b8b6f06e6848c37e2
Jonathan Piaget <piagetjonathan@protonmail.com>
2026-06-16 08:54:15 +0200

    chore(e2e): set up playwright test runner and config

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+	testDir: './e2e',
+	testMatch: '**/*.spec.ts',
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 1 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: [['list'], ['html', { open: 'never' }]],
+	use: {
+		trace: 'on-first-retry',
+	},
+});
